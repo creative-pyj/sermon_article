525a300c22198b309001316ea9587717546ff0f8
creative.pyj <creative.pyj@gmail.com>
2026-01-15 17:43:49 +0900

    127.0.0.1:8000以外の外部からのアクセス可にした。

--- sermon_article/sermon_article/settings.py
+++ sermon_article/sermon_article/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-!9do#8dm4u6!r5w1$lxksdco@fn(ye6xsm!vp7@l*s(4h6&7-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
